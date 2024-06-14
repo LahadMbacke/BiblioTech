4d8ccdc2a6e3a086f4ccabed9637bdcd9dfd73bf
Lahad Mbacke <lahadmbacke00@gmail.com>
2024-06-14 19:40:50 +0200

    add user in database

--- BiblioTech/library/views.py
+++ BiblioTech/library/views.py
@@ -8,20 +8,19 @@ from django.contrib.auth import authenticate, login as auth_login
 
 # Create your views here.
 
-# def home(request):
-#     return render(request, "library/home.html")
+def home(request):
+    return render(request, "library/home.html")
 
 def register(request):
     if request.method == "POST":
         form = forms.UserRegisterForm(request.POST)
         if form.is_valid():
-            user =form.save()
-            auth_login(request, user)
-            return redirect("home",forms.UserRegisterForm())
-        else:
-            user = forms.UserRegisterForm()
-            return render(request, "library/register.html", {"form": user})
-            
+            form.save()
+            return redirect("home")
+    else:
+        user = forms.UserRegisterForm()
+        return render(request, "library/register.html", {"form": user})
+        
 
 def login(request):
     if request.method == 'POST':
@@ -32,7 +31,7 @@ def login(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 auth_login(request, user)
-                return redirect('home')  # Remplacez 'home' par l'URL de votre choix
+                return render(request, "library/home.html", {"user": user})
     else:
         form = AuthenticationForm()
     return render(request, 'library/login.html', {'form': form})
